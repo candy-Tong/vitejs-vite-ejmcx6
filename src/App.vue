e9ef44b7afc5bb1b589e5d63f28dc6fb43408acd
candy-Tong <563378816@qq.com>
2022-11-01 19:59:14 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+//import 'vue-router';
+//import 'lodash-es';
+
+const count = ref(0);
+setInterval(() => {
+  count.value++;
+}, 1000);
+</script>
+
+<template>
+  <a-card title="antd title">
+    <h2>将 import 'lodash-es'; 取消注释</h2>
+    <h2>页面会刷新，count 会被重置</h2>
+    <button>{{ count }}</button>
+  </a-card>
+</template>
